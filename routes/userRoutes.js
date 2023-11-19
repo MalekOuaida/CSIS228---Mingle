bd2029185d6e7fd7cd0ef72bf70a573050408da4
MalekOuaida <74451913+MalekOuaida@users.noreply.github.com>
2023-11-20 00:13:30 +0200

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
-const { validateRegistration, validateLogin, validateUpdate } = require('../validators/userValidators');
+const { validateRegistration, validateUpdate } = require('../validators/userValidators');
 
 router.post('/register', validateRegistration, UserController.register);
-router.post('/login', validateLogin, UserController.login);
 router.put('/profile/:userId', validateUpdate, UserController.updateProfile);
 router.get('/profile/:userId', UserController.getProfile);
 router.delete('/:userId', UserController.deleteUser);
