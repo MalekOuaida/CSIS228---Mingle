5ce129db9ba043221f96faf7914e83b7c65acde1
MalekOuaida <74451913+MalekOuaida@users.noreply.github.com>
2023-11-21 21:30:06 +0200

    commit

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
